c6d0007133add2a889b5f4741e27e463a728e187
OsmanTunahan <osmantunahan007@gmail.com>
2024-06-26 14:00:44 +0300

    Add graphical user interface and animations

--- game.py
+++ game.py
@@ -15,6 +15,42 @@ class Movable(Drawable):
     def update(self, counter):
         pass
 
+class Dog(Movable):
+    def __init__(self, x, y):
+        super().__init__(DOG_IMAGE_PATH, x, y)
+        self.anime = False
+        self.direction = "UP"
+
+    def update(self, counter):
+        if self.anime:
+            if self.direction == "UP":
+                if counter % 5 == 0:
+                    self.y -= 20
+                if counter % 7 == 0:
+                    self.y += 10
+                if self.rect.top <= HEIGHT - 130:
+                    self.direction = "DOWN"
+            elif self.direction == "DOWN":
+                if counter % 5 == 0:
+                    self.y += 20
+                if counter % 7 == 0:
+                    self.y -= 10
+                if self.rect.top > HEIGHT:
+                    self.anime = False
+                    self.y = HEIGHT + 130
+                    self.direction = "UP"
+            self.rect = self.surf.get_rect(center=(self.x, self.y))
+
+    def start_animation(self):
+        self.anime = True
+
+class Bird(Drawable):
+    def __init__(self, x, y):
+        super().__init__(BIRD_IMAGE_PATH, x, y)
+
+    def reposition(self):
+        self.rect = self.surf.get_rect(center=(random.randint(50, WIDTH - 50), random.randint(50, HEIGHT - 50)))
+
 class Crosshair(Drawable):
     def update_position(self, pos):
         self.rect = self.surf.get_rect(center=pos)
@@ -51,6 +87,8 @@ class Game:
         self.running = True
         self.counter = 0
         self.points = 0
+        self.dog = Dog(WIDTH / 2, HEIGHT + 130)
+        self.bird = Bird(random.randint(50, WIDTH - 50), random.randint(50, HEIGHT - 50))
         self.crosshair = Crosshair(CROSSHAIR_IMAGE_PATH, WIDTH / 2, HEIGHT / 2)
         self.ammo = Ammo([pygame.image.load(path).convert_alpha() for path in AMMO_IMAGES_PATHS])
         self.font = pygame.font.SysFont('arial', 30, bold=True, italic=False)
@@ -91,12 +129,16 @@ class Game:
         if self.ammo.ammo == 0 and not self.ammo.reload_started:
             self.sound_manager.play_reload()
             self.ammo.start_reload()
+            self.dog.start_animation()
         if self.ammo.reload_started and self.counter % 200 == 0:
             self.ammo.reload()
+        self.dog.update(self.counter)
 
     def render(self):
         self.screen.blit(self.bg, (0, 0))
         self.ammo.draw(self.screen)
+        self.bird.draw(self.screen)
+        self.dog.draw(self.screen)
         self.screen.blit(self.text_surf, self.text_rect)
         self.crosshair.draw(self.screen)
         pygame.display.update()
